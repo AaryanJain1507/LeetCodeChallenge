810648348a7d3a7e2ec5931f01d56616a3b07afb
Aaryan Jain <142963995+AaryanJain1507@users.noreply.github.com>
2026-07-18 18:44:56 +0530

    Time: 0 ms (100%), Space: 42.4 MB (13.15%) - LeetHub

---
+++ 2413-smallest-even-multiple/2413-smallest-even-multiple.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int smallestEvenMultiple(int n) {
+        int ans = 0;
+        for(int i = 1; i <= n * 2; i++){
+            if(i%2 == 0 && i%n==0){
+                return i;
+            }
+        }
+        return 1;
+    }
+}
