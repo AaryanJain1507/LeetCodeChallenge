b7c021955b5190e6a2d03c55c1a55f87338d93cb
Aaryan Jain <142963995+AaryanJain1507@users.noreply.github.com>
2026-07-18 18:17:25 +0530

    Time: 0 ms (100%), Space: 45.1 MB (41.68%) - LeetHub

--- 1979-find-greatest-common-divisor-of-array/1979-find-greatest-common-divisor-of-array.java
+++ 1979-find-greatest-common-divisor-of-array/1979-find-greatest-common-divisor-of-array.java
@@ -8,12 +8,17 @@ class Solution {
             max = Math.max(max, num);
         }
 
-        for(int i = min; i >= 1; i--){
-            if(min % i == 0 && max % i == 0){
-                return i;
-            }
-        }
+        // for(int i = min; i >= 1; i--){
+        //     if(min % i == 0 && max % i == 0){
+        //         return i;
+        //     }
+        // }
 
-        return 1;
+        while(min!=0){
+            int temp = min;
+            min = max % min;
+            max = temp;
+        }
+        return max;
     }
 }
