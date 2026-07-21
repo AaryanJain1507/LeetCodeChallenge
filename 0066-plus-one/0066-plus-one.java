c9cd4cfba6ed10d5a0246f1606024796e56bd069
Aaryan Jain <142963995+AaryanJain1507@users.noreply.github.com>
2026-07-22 01:32:32 +0530

    Time: 0 ms (100%), Space: 43.3 MB (61.93%) - LeetHub

--- 0066-plus-one/0066-plus-one.java
+++ 0066-plus-one/0066-plus-one.java
@@ -1,15 +1,15 @@
 class Solution {
     public int[] plusOne(int[] digits) {
-        for(int i = digits.length - 1; i>=0; i--){
+        for(int i = digits.length -1; i >= 0; i--){
             if(digits[i]==9){
-                digits[i]=0;
+                digits[i] = 0;
             }
             else{
-                digits[i]+=1;
+                digits[i] += 1;
                 return digits;
             }
         }
-        int[]nums = new int[digits.length+1];
+        int[] nums = new int[digits.length+1];
         nums[0] = 1;
         return nums;
     }
