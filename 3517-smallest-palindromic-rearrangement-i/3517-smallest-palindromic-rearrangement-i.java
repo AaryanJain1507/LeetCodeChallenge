8b462073525d26f13ae3d92eb598b19131c25a15
Aaryan Jain <142963995+AaryanJain1507@users.noreply.github.com>
2026-07-28 13:45:28 +0530

    Time: 124 ms (7.23%), Space: 48.5 MB (10.04%) - LeetHub

---
+++ 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.java
@@ -0,0 +1,20 @@
+class Solution {
+    public String smallestPalindrome(String s) {
+        HashMap<Character, Integer> map = new HashMap<>();
+        StringBuilder left = new StringBuilder();
+        String middle = "";
+
+        for (char ch : s.toCharArray()) {
+            map.put(ch, map.getOrDefault(ch, 0) + 1);
+        }
+        for (char ch = 'a'; ch <= 'z'; ch++) {
+            int freq = map.getOrDefault(ch, 0);
+            if(freq%2!=0) middle = ch + "";
+            for (int i = 0; i < freq / 2; i++) {
+                left.append(ch);
+            }
+        }
+        String right = new StringBuilder(left).reverse().toString();
+        return left.toString() + middle + right;
+    }
+}
