620fada800d19ebb860773f0128c7c91526b8905
Aaryan Jain <142963995+AaryanJain1507@users.noreply.github.com>
2026-08-20 23:44:49 +0530

    Time: 2 ms (57.45%), Space: 46.6 MB (66.35%) - LeetHub

--- 3069-distribute-elements-into-two-arrays-i/3069-distribute-elements-into-two-arrays-i.java
+++ 3069-distribute-elements-into-two-arrays-i/3069-distribute-elements-into-two-arrays-i.java
@@ -5,21 +5,18 @@ class Solution {
         arr1.add(nums[0]);
         arr2.add(nums[1]);
         for(int i = 2; i < nums.length; i++){
-            if(arr1.get(arr1.size() - 1) > arr2.get(arr2.size() - 1)){
+            if (arr1.get(arr1.size() - 1) > arr2.get(arr2.size() - 1)) {
                 arr1.add(nums[i]);
-            }
-            else{
+            } else {
                 arr2.add(nums[i]);
             }
         }
         int[] result = new int[nums.length];
         int index = 0;
-
-        for (int num : arr1) {
+        for(int num : arr1){
             result[index++] = num;
         }
-
-        for (int num : arr2) {
+        for(int num : arr2){
             result[index++] = num;
         }
         return result;
