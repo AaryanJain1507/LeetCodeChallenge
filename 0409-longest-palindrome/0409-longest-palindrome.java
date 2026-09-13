cb701670975e8166017566d8f98bddd66b8d0528
Aaryan Jain <142963995+AaryanJain1507@users.noreply.github.com>
2026-09-13 14:46:00 +0530

    Time: 1 ms (99.87%), Space: 43.3 MB (26.99%) - LeetHub

---
+++ 0409-longest-palindrome/0409-longest-palindrome.java
@@ -0,0 +1,29 @@
+class Solution {
+
+    public int longestPalindrome(String s) {
+
+        int[] count = new int[128];
+
+        for(char ch : s.toCharArray()){
+            count[ch]++;
+        }
+
+        int length = 0;
+        boolean odd = false;
+
+        for(int n : count){
+
+            length += (n / 2) * 2;
+
+            if(n % 2 == 1){
+                odd = true;
+            }
+        }
+
+        if(odd){
+            length++;
+        }
+
+        return length;
+    }
+}
