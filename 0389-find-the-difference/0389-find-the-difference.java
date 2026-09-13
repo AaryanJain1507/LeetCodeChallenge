0fbb3a28103d2c94762df705c716c1012c344a52
Aaryan Jain <142963995+AaryanJain1507@users.noreply.github.com>
2026-09-13 14:38:16 +0530

    Time: 2 ms (66.14%), Space: 43 MB (84.53%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.java
+++ 0389-find-the-difference/0389-find-the-difference.java
@@ -1,17 +1,38 @@
+// class Solution {
+//     public char findTheDifference(String s, String t) {
+//         HashMap<Character, Integer> map = new HashMap<>();
+//         for(char ch : s.toCharArray()){
+//             map.put(ch, map.getOrDefault(ch, 0) + 1);
+//         }
+//         for(char ch : t.toCharArray()){
+//             if(map.getOrDefault(ch, 0) >= 1){
+//                 map.put(ch, map.get(ch) - 1);
+//             }
+//             else{
+//                 return ch;
+//             }
+//         }
+//         return ' ';
+//     }
+// }
 class Solution {
+
     public char findTheDifference(String s, String t) {
-        HashMap<Character, Integer> map = new HashMap<>();
+
+        int[] count = new int[26];
+
         for(char ch : s.toCharArray()){
-            map.put(ch, map.getOrDefault(ch, 0) + 1);
+            count[ch - 'a']++;
         }
+
         for(char ch : t.toCharArray()){
-            if(map.getOrDefault(ch, 0) >= 1){
-                map.put(ch, map.get(ch) - 1);
-            }
-            else{
+            count[ch - 'a']--;
+
+            if(count[ch - 'a'] < 0){
                 return ch;
             }
         }
+
         return ' ';
     }
 }
